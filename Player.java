94d2e9aca28e0b45f8f83b3f19ddd332d3355a46
Fendy Yurista <yuristafendy@gmail.com>
2025-06-04 23:57:06 +0700

    Create Player.java
    Dibagi untuk memegang tanggung jawab Player

---
+++ Player.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Card> hand = new ArrayList<>();
+    private ArrayList<Card> palette = new ArrayList<>();
+
+    public Player clonePlayer() {
+        Player copy = new Player();
+        for (Card card : this.hand) {
+            copy.hand.add(new Card(card.getColor(), card.getNumber()));
+        }
+        for (Card card : this.palette) {
+            copy.palette.add(new Card(card.getColor(), card.getNumber()));
+        }
+        return copy;
+    }
+
+    public static <T> void replaceContentsWithAnother(ArrayList<T> toReplace, ArrayList<T> newContents) {
+        toReplace.clear();
+        toReplace.addAll(newContents);
+    }
+
+    public void copyFrom(Player other) {
+        replaceContentsWithAnother(this.hand, other.hand);
+        replaceContentsWithAnother(this.palette, other.palette);
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public ArrayList<Card> getPalette() {
+        return palette;
+    }
+
+    public Card removeCardFromHand(int index) {
+        return hand.remove(index);
+    }
+
+    public void addCardToPalette(Card card) {
+        palette.add(card);
+    }
+}
